6ee0369635553efef83dd5fbfdecd6338dc8e715
ASHISH SINGHAL <ashish.ash216@gmail.com>
2020-01-18 19:58:53 +0100

    This file will do clustering for big and mini

--- cluster.py
+++ cluster.py
@@ -6,7 +6,7 @@ from pyspark.ml.clustering import KMeans
 spark = SparkSession.builder.getOrCreate()
 
 #Reading points
-pointsData    = spark.read.csv("/user/s2279444/PointsResult/*")
+pointsData    = spark.read.csv("/user/s2279444/Cluster3/*")
 pointsData    = pointsData.selectExpr("_c0 as Latitudes", "_c1 as Longitudes")
 pointsData    = pointsData.withColumn("Latitudes", pointsData["Latitudes"].cast("double"))
 pointsData    = pointsData.withColumn("Longitudes", pointsData["Longitudes"].cast("double"))
@@ -16,12 +16,16 @@ columns       = pointsData.columns
 assembler     = VectorAssembler(inputCols=columns,outputCol="features")
 dataset       = assembler.transform(pointsData)
 
-kValue        = 5
+kValue        = 4
 kmeans        = KMeans().setK(kValue).setSeed(1)
 model         = kmeans.fit(dataset)
 predictions   = model.transform(dataset)
 
-finalData     = predictions.select("features", "prediction")
+finalData     = predictions.select("Latitudes","Longitudes", "prediction")
 
 for i in range(0,kValue):
 	data = finalData.filter(finalData["prediction"] == i)
+	data = data.select(data["Latitudes"], data["Longitudes"])
+	dirName = "Cluster33_"+str(i)
+	print("ASHISH: Running for cluster... dirName ->",dirName)
+	data.write.csv(dirName)
